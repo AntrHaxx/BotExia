106c4b60942148dbacad96b3d842e93333b748f6
AntrHaXx <antrhaxx@localhost.localdomain>
2018-03-18 20:43:37 +0100

    commande info reparee et mise aux normes

--- commands/standard/info.js
+++ commands/standard/info.js
@@ -1,50 +1,44 @@
-const Discord = require('discord.js');
-
 module.exports = {
-    name : "info",
-    description : "Info relative à une commande precise",
-    usage : "`*info <NomDeLaCommande>`",
-    execute(message, args, client){
-        const arg = message.content.split(/ +/);
-        const command = arg[0];
-
-        const embed = {
-            "title": "Info",
-            "color": 4886754,
-           
-            "footer": {
-              "text": "Exia.cesi Promo St-Nazaire-2017"
-            },
-            "author": {
-              "name": "Bot Exia v2.0",
-            },
-            "fields": [
-              {
-                "name" : "Nom",
-                "value" : "Nom de la commande : " +client.commands.standard.name || client.commands.prosit.name
-              },
-              {
-                "name": "Description",
-                "value" :  ""+client.commands.standard.description || client.commands.prosit.description
-              },
-              {
-                "name" : "Commande",
-                "value" : ""+client.commands.standard.usage || client.command.prosit.usage
-              },
-            ]
-          };
-          
-        if (!args.length) {
-            message.channel.send('N\'oulis pas de mettre le nom de la commande');
-        }
-        else {
-            if(!client.commands.standard.command || !client.commands.prosit.command){
-                message.channel.send("Ce n\'est pas une commande valide")
-            }
-            else{
-                message.channel.send({embed})
-            }
-    
-        }
+  name : "info",
+  description : "Info relative à une commande precise",
+  usage : "`*info <NomDeLaCommande>`",
+  execute(message, args, client){
+    if (!args.length)
+      return Msg.error('N\'oulis pas de mettre le nom de la commande');
+    let command = null;
+    for (category in client.commands)
+    {
+        if (command == null)
+            command = client.commands[category].get(args[0]);
+        else
+            break;
     }
+    if (command == null)
+      return Msg.error("Ce n\'est pas une commande valide");
+    Msg.format({
+      "title": "Info",
+      "color": "info",
+
+      "footer": {
+        "text": "Exia.cesi Promo St-Nazaire-2017"
+      },
+      "author": {
+        "name": "Bot Exia v2.0",
+      },
+      "fields": [
+        {
+          "name" : "Nom",
+          "value" : command.name
+        },
+        {
+          "name": "Description",
+          "value" :  ""+command.description
+        },
+        {
+          "name" : "Commande",
+          "value" : ""+command.usage
+        }
+      ]
+    });
+  }
 }
