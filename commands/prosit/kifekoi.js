0e0c559abd6bff6895c7a6346636832066e02e34
CptBaroud <gurvan.seveno@gmail.com>
2018-03-19 19:11:03 +0100

    Prosit 0.5

--- commands/prosit/kifekoi.js
+++ commands/prosit/kifekoi.js
@@ -1,140 +1,155 @@
-exports = {
+module.exports = {
     name : "kifekoi",
     description : "Sa sert pus a rien",
-	usage : "`*kifekoi`",
-	alias : ["role" ],
+		usage : "`*kifekoi`",
+		alias : ["role"],
+		permissions: {
+        "*": "*"
+    },
     execute(message){
+		console.log("La Commande elle marche presque");
 
-        const { prosit, un, deux, trois, quatres, cinq, six, sept, huit, neuf, dix, onze, douze, animateur, secretaire, scribe, inutile } = require("../../json/pr.json");
-        const { pro } = require("../../json/prosit.json")
+    const { pr, un, deux, trois, quatres, cinq, six, sept, huit, neuf, dix, onze, douze, animateur, secretaire, scribe, inutile } = require("../../json/pr.json");
+		const { pro } = require("../../json/prosit.json")
 		
-		message.channel.send(""+pro);
+		var prositAller = client.channels.find("name", "role");
+		var anim;
+		var scr;
+		var sec;
+		var inu;		
 
-        function changement()
-{
-    switch(pro%12)
+		var temp = parseFloat(pro);
+
+		console.log("Sa marche");
+
+  function changement()
+	{
+    switch(temp%12)
 		{
 		//Prosit 1 
 			case 1 :
-				anim = pr.un.animateur;
-				sec = pr.un.secretaire;
-				scr = pr.un.scribe;
-				inu = pr.un.inutile;
+				anim = un.animateur;
+				sec = un.secretaire;
+				scr = un.scribe;
+				inu = un.inutile;
 			break;
         //Prosit 2
 			case 2 :
-				anim = pr.deux.animateur;
-				sec = pr.deux.secretaire;
-				scr = pr.deux.scribe;
-				inu = pr.deux.inutile;
+				anim = deux.animateur;
+				sec = deux.secretaire;
+				scr = deux.scribe;
+				inu = deux.inutile;
 			break;
         //Prosit 3
 			case 3 :
-			anim = pr.trois.animateur;
-			sec = pr.trois.secretaire;
-			scr = pr.trois.scribe;
-			inu = pr.trois.inutile;
+			anim = trois.animateur;
+			sec = trois.secretaire;
+			scr = trois.scribe;
+			inu = trois.inutile;
 			break;
         //prosit 4
 			case 4 :
-			anim = pr.quatres.animateur;
-			sec = pr.quatres.secretaire;
-			scr = pr.quatres.scribe;
-			inu = pr.quatres.inutile;
+			anim = quatres.animateur;
+			sec = quatres.secretaire;
+			scr = quatres.scribe;
+			inu = quatres.inutile;
 			break;
         //Prosit 5
 			case 5 :
-		    anim = pr.cinq.animateur;
-			sec = pr.cinq.secretaire;
-			scr = pr.cinq.scribe;
-			inu = pr.cinq.inutile;
+		  anim = cinq.animateur;
+			sec = cinq.secretaire;
+			scr = cinq.scribe;
+			inu = cinq.inutile;
 			break;
         //Prosit 6 
 			case 6 :
-			anim = pr.six.animateur;
-			sec = pr.six.secretaire;
-			scr = pr.six.scribe;
-			inu = pr.six.inutile;
+			anim = six.animateur;
+			sec = six.secretaire;
+			scr = six.scribe;
+			inu = six.inutile;
 			break;
 		//prosit 7 	
 			case 7 :
-			anim = pr.sept.animateur;
-			sec = pr.sept.secretaire;
-			scr = pr.sept.scribe;
-			inu = pr.sept.inutile;
+			anim = sept.animateur;
+			sec = sept.secretaire;
+			scr = sept.scribe;
+			inu = sept.inutile;
 			break;
 		//Prosit 8 	
 			case 8 :
-			anim = pr.huit.animateur;
-			sec = pr.huit.secretaire;
-			scr = pr.huit.scribe;
-			inu = pr.huit.inutile;
+			anim = huit.animateur;
+			sec = huit.secretaire;
+			scr = huit.scribe;
+			inu = huit.inutile;
 			break;
         //Prosit 9
 			case 9 :
-			anim = pr.neuf.animateur;
-			sec = pr.neuf.secretaire;
-			scr = pr.neuf.scribe;
-			inu = pr.neuf.inutile;
+			anim = neuf.animateur;
+			sec = neuf.secretaire;
+			scr = neuf.scribe;
+			inu = neuf.inutile;
 			break;
 		//Prosit 10	
 			case 10 :
-			anim = pr.dix.animateur;
-			sec = pr.dix.secretaire;
-			scr = pr.dix.scribe;
-			inu = pr.dix.inutile;
+			anim = dix.animateur;
+			sec = dix.secretaire;
+			scr = dix.scribe;
+			inu = dix.inutile;
 			break;
 		//Prosit 11
 			case 11 :
-			anim = pr.onze.animateur;
-			sec = pr.onze.secretaire;
-			scr = pr.onze.scribe;
-			inu = pr.onze.inutile;
+			anim = onze.animateur;
+			sec = onze.secretaire;
+			scr = onze.scribe;
+			inu = onze.inutile;
 			break;
         //Prosit 12 
 			case 12 :
-			anim = pr.douze.animateur;
-			sec = pr.douze.secretaire;
-			scr = pr.douze.scribe;
-			inu = pr.douze.inutile;
+			anim = douze.animateur;
+			sec = douze.secretaire;
+			scr = douze.scribe;
+			inu = douze.inutile;
 			break;
 			default : 
 			console.log("pr : " +pro);
-            break;
-        }
-}
+      break;
+  	}
+	}
 
-        const embed = {
-            "title": "Kifekoi",
-            "color": 2E4756,
-           
-            "footer": {
-              "text": "Exia.cesi Promo St-Nazaire-2017"
-            },
-            "author": {
-              "name": "Bot Exia v2.0",
-            },
-            "fields": [
-              {
-                "name" : "Animateur",
-                "value" : "Ce coup si l'animateur c'est : " +animateur
-              },
-              {
-                "name": "Secretaire",
-                "value" :  "Bon aller go au tableaux "+secretaire
-              },
-              {
-                "name" : "Scribe",
-                "value" : "Aller hop hop hop on sort word et on prend en note "+scribe +"PS : t'a la commande `*template` y'a plus qu'a remplir"
-              },
-              {
-                  "name" : "Gestionaire",
-                  "value" : "Et ce coup la c'est a ton tour de se tourner les pouces : " +inutile
-              }
-            ]
-          };
+  const embed = {
+		"title": "Kifekoi",
+		"color": 2E4756, 
+	 
+		"footer": {
+			"text": "Exia.cesi | Promo St-Nazaire-2017"
+		},
+		"author": {
+			"name": "Bot Exia v2.0",
+			"icon_url" : "https://cdn.discordapp.com/attachments/400651374146355210/425232194773057536/color4.jpg"
+		},
+		"fields": [
+			{
+				"name" : "Animateur",
+				"value" : "Ce coup ci l'animateur c'est : " +anim
+			},
+			{
+				"name": "Secretaire",
+				"value" :  "Bon aller go au tableau " +sec
+			},
+			{
+				"name" : "Scribe",
+				"value" : "Aller hop hop hop on sort word et on prend en note " +scr +"\nPS : tu as la commande `*template` y'a plus qu'a remplir"
+			},
+			{
+				"name" : "Gestionaire",
+				"value" : "Et ce coup là c'est à ton tour de te tourner les pouces : " +inu
+			}
+		]
+	};     
 
-          changement();
-          message.channel.send({embed})
-    },
+  changement();
+	prositAller.send({embed})
+	message.reply("SALUT "+onze.animateur);
+	message.reply("Pro " +(temp%12));
+  },
 }
